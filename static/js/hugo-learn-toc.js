d4efa97193ce6317ad2678b52be2a04f26ea4a42
famaridon <famaridon@visiativ.com>
2018-01-05 14:22:44 +0100

    add aside toc pretty scrollspy

--- static/js/hugo-learn-toc.js
+++ static/js/hugo-learn-toc.js
@@ -37,5 +37,5 @@ function initAsideToc() {
             elementToHide.classList.add('d-none');
         });
     }
-    // initScrollSpy();
+    initScrollSpy(toc);
 }
--- static/js/scrollspy.js
+++ static/js/scrollspy.js
@@ -1,22 +1,28 @@
 'use strict';
 
-function initScrollSpy() {
+function initScrollSpy(toc) {
     const section = document.querySelectorAll('h2, h3, h4, h5, h6');
     const sections = {};
+    var i = 0;
 
     Array.prototype.forEach.call(section, function (e) {
-        sections[e.id] = e.offsetTop - 62;
-        e.setAttribute('data-offsetTop', sections[e.id]);
+        if (e.id) {
+            sections[e.id] = e.offsetTop - 62;
+            e.setAttribute('data-offsetTop', sections[e.id]);
+        }
     });
 
     window.onscroll = function () {
         const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-        for (var i in sections) {
+        for (i in sections) {
             if (sections[i] <= scrollPosition) {
-                document.querySelectorAll('.active').forEach(function(e) {
+                document.querySelectorAll('.active').forEach(function (e) {
                     e.classList.remove('active');
                 });
-                const active = document.querySelector('.toc a[href=\'#' + i + '\']');
+                const active = toc.querySelector('a[href=\'#' + i + '\']');
+                if (!active) {
+                    return;
+                }
                 active.classList.add('active');
 
                 var parent = active.parentElement;
