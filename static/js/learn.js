546f0826addc0af9288821a9fdaa84848fd89655
Swen Wenzel <swen.wenzel@real-digital.de>
2020-03-20 10:49:17 +0100

    some code optimization

--- static/js/learn.js
+++ static/js/learn.js
@@ -51,7 +51,12 @@ function switchTab(tabGroup, tabId) {
     allTabItems = jQuery("[data-tab-group='"+tabGroup+"']");
     targetTabItems = jQuery("[data-tab-group='"+tabGroup+"'][data-tab-item='"+tabId+"']");
 
-    if(event != undefined){
+    // if event is undefined then switchTab was called from restoreTabSelection
+    // so it's not a button event and we don't need to safe the selction or
+    // prevent page jump
+    var isButtonEvent = event != undefined;
+
+    if(isButtonEvent){
       // save button position relative to viewport
       var yposButton = event.target.getBoundingClientRect().top;
     }
@@ -59,22 +64,22 @@ function switchTab(tabGroup, tabId) {
     allTabItems.removeClass("active");
     targetTabItems.addClass("active");
 
-    if(event != undefined){
+    if(isButtonEvent){
       // reset screen to the same position relative to clicked button to prevent page jump
       var yposButtonDiff = event.target.getBoundingClientRect().top - yposButton;
       window.scrollTo(window.scrollX, window.scrollY+yposButtonDiff);
-    }
 
-    // Store the selection to make it persistent
-    if(window.localStorage){
-        var selectionsJSON = window.localStorage.getItem("tabSelections");
-        if(selectionsJSON){
-          var tabSelections = JSON.parse(selectionsJSON);
-        }else{
-          var tabSelections = {};
-        }
-        tabSelections[tabGroup] = tabId;
-        window.localStorage.setItem("tabSelections", JSON.stringify(tabSelections));
+      // Store the selection to make it persistent
+      if(window.localStorage){
+          var selectionsJSON = window.localStorage.getItem("tabSelections");
+          if(selectionsJSON){
+            var tabSelections = JSON.parse(selectionsJSON);
+          }else{
+            var tabSelections = {};
+          }
+          tabSelections[tabGroup] = tabId;
+          window.localStorage.setItem("tabSelections", JSON.stringify(tabSelections));
+      }
     }
 }
 
