6f88cd6b9ac98117313f89784778b0441d80dff4
Jylpah <jylpah@gmail.com>
2020-04-27 10:01:02 +0300

    Fix "type o is undefined" JS error

--- static/js/hugo-learn.js
+++ static/js/hugo-learn.js
@@ -22,11 +22,13 @@ var images = $("div#body-inner img").not(".inline");
 images.wrap(function(){
   var image =$(this);
   var o = getUrlParameter(image[0].src);
-  var f = o['featherlight'];
-  // IF featherlight is false, do not use feather light
-  if (f != 'false') {
-    if (!image.parent("a").length) {
-      return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
+  if (typeof o !== "undefined") {
+    var f = o['featherlight'];
+    // IF featherlight is false, do not use feather light
+    if (f != 'false') {
+      if (!image.parent("a").length) {
+        return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
+      }
     }
   }
 });
